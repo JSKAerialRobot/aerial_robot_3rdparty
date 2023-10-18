34930ff60f294055105294e42890c253e6d7e80a
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-10-18 21:06:44 +0900

    [Mujoco] modify the macro function for model conversion

--- mujoco_ros_control/cmake/model_convert.cmake
+++ mujoco_ros_control/cmake/model_convert.cmake
@@ -1,9 +1,8 @@
-macro(mujoco_model_convert _PROJECT_SOURCE_DIR _URDF_DIR _YAML_FILE)
+macro(mujoco_model_convert _PROJECT_SOURCE_DIR _YAML_FILE)
   add_custom_command(OUTPUT ${_PROJECT_SOURCE_DIR}/mujoco
     COMMAND rosrun mujoco_ros_control mujoco_model_generator.py ${_YAML_FILE}
     POST_BUILD
-    WORKING_DIRECTORY ${_PROJECT_SOURCE_DIR}
-    DEPENDS ${_URDF_DIR}  ${_YAML_FILE})
+    WORKING_DIRECTORY ${_PROJECT_SOURCE_DIR})
 
   add_custom_target(generate_mujoco_model ALL DEPENDS ${_PROJECT_SOURCE_DIR}/mujoco)
-endmacro(mujoco_model_convert _PROJECT_SOURCE_DIR)
+endmacro(mujoco_model_convert _PROJECT_SOURCE_DIR _YAML_FILE)
