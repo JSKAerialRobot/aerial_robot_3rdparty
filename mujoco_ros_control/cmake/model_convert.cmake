37a7430e458c195b4af02da9feb9563b031c5938
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-10-18 21:12:26 +0900

    [Mujoco] do not convert the mujoco model from urdf in ubuntu 16.04 because of the old version of blender

--- mujoco_ros_control/cmake/model_convert.cmake
+++ mujoco_ros_control/cmake/model_convert.cmake
@@ -1,8 +1,22 @@
 macro(mujoco_model_convert _PROJECT_SOURCE_DIR _YAML_FILE)
-  add_custom_command(OUTPUT ${_PROJECT_SOURCE_DIR}/mujoco
-    COMMAND rosrun mujoco_ros_control mujoco_model_generator.py ${_YAML_FILE}
-    POST_BUILD
-    WORKING_DIRECTORY ${_PROJECT_SOURCE_DIR})
 
-  add_custom_target(generate_mujoco_model ALL DEPENDS ${_PROJECT_SOURCE_DIR}/mujoco)
+  find_program(LSB_RELEASE_EXEC lsb_release)
+  execute_process(COMMAND ${LSB_RELEASE_EXEC} -c
+    OUTPUT_VARIABLE LSB_RELEASE_CODENAME
+    )
+
+  string(REPLACE "\t" ";" LSB_RELEASE_CODENAME_LIST ${LSB_RELEASE_CODENAME})
+  list(GET LSB_RELEASE_CODENAME_LIST 1 CODENAME)
+
+  if(${CODENAME} MATCHES "xenial")
+    MESSAGE(WARNING "Do not convert mujoco model in ${CODENAME}, because of old version of blender")
+  else()
+
+    add_custom_command(OUTPUT ${_PROJECT_SOURCE_DIR}/mujoco
+      COMMAND rosrun mujoco_ros_control mujoco_model_generator.py ${_YAML_FILE}
+      POST_BUILD
+      WORKING_DIRECTORY ${_PROJECT_SOURCE_DIR})
+
+    add_custom_target(generate_mujoco_model ALL DEPENDS ${_PROJECT_SOURCE_DIR}/mujoco)
+  endif()
 endmacro(mujoco_model_convert _PROJECT_SOURCE_DIR _YAML_FILE)
